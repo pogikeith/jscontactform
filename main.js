7a091bcfdee27b010ce9bae03db86e1f2e1bcaf7
pogikeith <pogikeith@yahoo.com>
2020-01-13 16:21:18 -0700

    email

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 console.log('it works')
 
-$(document).ready(function (){
+$(document).ready(function () {
     $('.submit').click(function(event) {
-        event.preventDefault()
+       
         console.log('clicked button')
-}) 
+
+        var email = $('.email').val()
+        var subject = $('.subject').val()
+        var subject = $('.message').val()
+        var statusElm = $('.status')
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>email is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>email is not valid</div>')
+        }    
+
+        if(subject.length >= 2) {
+            statusElm.append('<div>Subject is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Subject is not valid</div>')
+        }   
+        
+        if(message.length > 20) {
+            statusElm.append('<div>Message is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+    }) 
 
 }) 
